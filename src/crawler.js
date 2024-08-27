6bbacc3a2e3466135317fb195137402a241bf46f
son.nguyentruong1 <sonqp1999a@gmail.com>
2024-08-27 10:16:58 +0700

    filter by address

--- src/crawler.js
+++ src/crawler.js
@@ -8,12 +8,14 @@ const address = "0x316Fb96Cbe2fB52Dbe679d75B928FCFad858241B ";
 async function getTransactions(address) {
   try {
     const latestBlockNumber = Number(await web3.eth.getBlockNumber());
-    for (let i = 0; i <= latestBlockNumber; i++) {
-      const block = await web3.eth.getBlock(latestBlockNumber - i, true);
+    for (let i = latestBlockNumber; i >= 0; i--) {
+      const block = await web3.eth.getBlock(i, true);
       if (block?.transactions) {
         for (const tx of block.transactions) {
-          const data = await web3.eth.getTransaction(tx.hash);
-          console.log(`transactionHash ${tx.hash}:`, data);
+          if (tx.from === address || tx.to === address) {
+            const data = await web3.eth.getTransaction(tx.hash);
+            console.log(`transactionHash ${tx.hash}:`, data);
+          }
         }
       }
     }
